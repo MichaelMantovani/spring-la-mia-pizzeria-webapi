d891404bb87351b3874404729db610dda12c8fe0
MichaelMantovani <michmantovani@gmail.com>
2023-12-14 15:32:43 +0100

    create front-end and add an index page

---
+++ front-end/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+import HomePage from './components/HomePage.vue';
+
+const pizze = ref(null);
+
+const getPizze = async () => {
+  const data = await axios.get('http://localhost:8080/api/pizza');
+
+  pizze.value = data.data;
+};
+
+onMounted(getPizze);
+</script>
+
+<template>
+  <div><HomePage :pizze="pizze" /></div>
+</template>
---
+++ front-end/src/components/HomePage.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  pizze: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="container mt-5">
+    <div class="d-flex gap-3">
+      <form class="d-flex mb-4" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" name="searchValue" />
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+      <div>
+        <a href="#" class="btn btn-primary">Crea pizza</a>
+      </div>
+    </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Nome</th>
+          <th scope="col">Ingredienti</th>
+          <th scope="col">Prezzo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="pizza in pizze" :key="pizza.id">
+          <th>{{ pizza.name }}</th>
+          <td>
+            <ul class="d-flex list-unstyled gap-1">
+              <li v-for="ingrediente in pizza.ingredienti">{{ ingrediente.name }},</li>
+            </ul>
+          </td>
+          <td>{{ pizza.prezzo }} €</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
